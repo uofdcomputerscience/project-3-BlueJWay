242a035d36d8d9a8a564b9cc050a48f494d1aca5
Brendan Flood <brendan_flood@yahoo.com>
2019-12-05 21:38:09 -0600

    Including ReviewCell this time

---
+++ Project3Sample/ReviewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewCell.swift
+//  Project3Sample
+//
+//  Created by Brendan Flood on 12/4/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var reviewer: UILabel!
+    @IBOutlet weak var book: UILabel!
+    @IBOutlet weak var body: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var viewButton: ReviewButton!
+    
+}
